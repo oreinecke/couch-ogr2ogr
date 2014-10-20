3c8fdfade51efe1173bef8e2f1191f9324535bb0
Oscar Reinecke <reinecke@posteo.de>
2014-10-20 13:58:25 +0200

    Added plugin handler for GET,POST.
    For GET it validates roles, config and responds with the ogr2ogr help page
    (useless).
    For POST it validates roles, config, content type and expects a JSON object
    with the extension as key and base64-encoded content as value:
    {
      "shx": "AAAnCgAAAAAAAAAAAAAAAAAAAAAAAA etc. etc. AAAAAAAAAAAAAAAAADIAAAAK",
      "shp": "AAAnCgAAAAAAAAAAAAAAAAA etc. etc. KUVpslRBUsnDnUN8VUE=",
      "qpj": "UFJPSkNTWyJHZXJtYW55X1 etc. etc. FPUklUWVsiRVBTRyIsIjMxNDk1Il1dCg==",
      "prj": "UFJPSkNTWyJHZXJtYW55X1 etc. etc. 1d",
      "dbf": "A18HGgEAAABhAKEAAAAAAAA etc. etc. etc. AgICAgICAgICAgICAgICAgICAg"
    }
    It then writes every file in a temp dir as "remove-me.<extension>", runs
    
    $ ogr2ogr -f GeoJSON <tmp-dir>/remove-me.geojson <tmp-dir>/remove-me.shp
    
    and returns the contents of <tmp-dir>/remove-me.geojson.

---
+++ src/couch_ogr2ogr_httpd.erl
@@ -0,0 +1,74 @@
+-module(couch_ogr2ogr_httpd).
+
+-export([handle_req/1, get_allowed_roles/0]).
+
+-include_lib("couch/include/couch_db.hrl").
+
+handle_req(#httpd{method='POST'}=Req) ->
+  verify_roles(Req),
+  validate_config(),
+  couch_httpd:validate_ctype(Req, "application/json"),
+  TempDir = mochitemp:mkdtemp(),
+  BaseName = filename:join(TempDir, "remove-me"),
+  {Props} = couch_httpd:json_body_obj(Req),
+  lists:foreach( fun({Extension, Content}) ->
+    file:write_file(BaseName ++ "." ++ binary_to_list(Extension), base64:decode(Content))
+  end, Props ),
+  os:cmd(get_command() 
+    ++ " -f GeoJSON " 
+    ++ case proplists:is_defined(<<"qpj">>, Props) of
+      true -> " -a_srs " ++ BaseName ++ ".qpj ";
+      false -> "" end
+    ++ BaseName ++ ".geojson " 
+    ++ BaseName ++ ".shp"
+  ),
+  {ok, GeoJSON} = file:read_file( BaseName ++ ".geojson"),
+  mochitemp:rmtempdir(TempDir),
+  couch_httpd:send_response(Req, 200, [{"Content-type", "application/json;charset=utf-8"}], GeoJSON);
+
+handle_req(#httpd{method='GET'}=Req) ->
+  verify_roles(Req),
+  validate_config(),
+  couch_httpd:send_response(Req, 200,
+    [{"Content-type", "application/json;charset=utf-8"}],
+    os:cmd(get_command())
+  );
+
+handle_req(Req) ->
+  couch_httpd:send_method_not_allowed(Req, "GET,POST").
+
+get_command() ->
+  case couch_config:get("ogr2ogr", "command") of
+    "null" -> undefined;
+    "undefined" -> undefined;
+    Command -> Command
+  end.
+
+validate_config() ->
+  case get_command() of 
+    undefined -> throw({internal_server_error, "Undefined path to ogr2ogr comand."});
+    _-> ok
+  end.
+
+get_allowed_roles() ->
+  case couch_config:get("ogr2ogr", "roles") of
+    undefined -> [];
+    Roles -> [ list_to_binary(Role) || Role <- string:tokens(Roles, ", ") ]
+  end.
+
+verify_roles(#httpd{user_ctx=UserCtx}) ->
+  verify_roles(UserCtx);
+verify_roles(#user_ctx{roles=Roles}) ->
+  case get_allowed_roles() of
+    [] -> ok;
+    AllowedRoles -> verify_roles( Roles, [ <<"_admin">> | AllowedRoles] )
+  end.
+verify_roles(Roles, [ Role | AllowedRoles] ) -> 
+  case lists:member(Role, Roles) of
+    true -> ok;
+    _ -> verify_roles(Roles, AllowedRoles)
+  end;
+verify_roles(_Roles, [] ) -> 
+  throw({unauthorized, "You must be "
+        ++ couch_config:get("ogr2ogr", "roles") 
+        ++ " or admin to use this service."}).
